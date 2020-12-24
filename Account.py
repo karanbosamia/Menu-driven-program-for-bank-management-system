a010ea246c3ae95ef542a8c742fdad63c88f7361
karanbosamia <76547251+karanbosamia@users.noreply.github.com>
2020-12-24 12:11:14 +0530

    Created a function for displaying account details
    Created a function for displaying account details every time when a method like deposit, withdrawal or creation of account is done.
    Edited the error for the length of account number in __init__ function and resolved it, introduced print statements along with deposit, withdrawal and account creation functions for better readablility

--- Account.py
+++ Account.py
@@ -1,26 +1,28 @@
 class Account:
 
     def __init__(self, acc_no, name, balance):
-        if len(acc_no) == 12 or len(acc_no) == 14:
-            self._acc_no = acc_no
-        else:
-            print("Account can't be created as your account no. isn't a 12 digit or a 14 digit number")
-            break
+        self._acc_no = acc_no
         self._name = name
         self._balance = balance
         print("Account created for " + self._name)
         self.display_account()
+        print("*" * 40)
     
     def deposit(self, amount):
         if amount > 0:
             self._balance += amount
+            print("The amount deposited in your account is {} Rs.".format(amount))
             self.display_account()
+            print("*" * 40)
     
     def withdraw(self, amount):
         if 0 <= amount <= self._balance:
             self._balance -= amount
+            print("The amount withdrawn from your account is {} Rs.".format(amount))
         else:
-            print("The amount must be greater than zero and should not be higher than your account balance")
+            print("The amount must be greater than zero or it should not be higher than your account balance")
         self.display_account()
+        print("*" * 40)
         
-     
+    def display_account(self):
+        print("Account name: {}\nAccount number: {}\nBalance: {}".format(self._name, self._acc_no, self._balance))
