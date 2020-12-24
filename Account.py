905923b6af4e86c27d4ef02bfc1c3dd887530dbf
karanbosamia <76547251+karanbosamia@users.noreply.github.com>
2020-12-24 11:51:25 +0530

    Defined withdraw function
    Defined withdraw function:
    withdraw(amount)
    and withdraw is set to conditions that if the amount is between zero and account balance then only the amount can be withdrawn
    Also, edited the changes that if the length of account number is equal to 12 or 14 then only account will be created

--- Account.py
+++ Account.py
@@ -1,7 +1,11 @@
 class Account:
 
     def __init__(self, acc_no, name, balance):
-        self._acc_no = acc_no
+        if len(acc_no) == 12 or len(acc_no) == 14:
+            self._acc_no = acc_no
+        else:
+            print("Account can't be created as your account no. isn't a 12 digit or a 14 digit number")
+            break
         self._name = name
         self._balance = balance
         print("Account created for " + self._name)
@@ -11,3 +15,12 @@ class Account:
         if amount > 0:
             self._balance += amount
             self.display_account()
+    
+    def withdraw(self, amount):
+        if 0 <= amount <= self._balance:
+            self._balance -= amount
+        else:
+            print("The amount must be greater than zero and should not be higher than your account balance")
+        self.display_account()
+        
+     
